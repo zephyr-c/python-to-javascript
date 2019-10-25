b3518720565d05def5a176f01981bf2c24644bb2
Zephyr Worthington <z.worthinton@outlook.com>
2019-10-25 04:14:26 +0000

    translate melon_counting to melonCounting.js

---
+++ melonCounting.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+
+const countMelons = (melonList) => {
+
+    const melonCounts = {};
+
+    for (const melon of melonList){
+        if (!melonCounts[melon]){
+            melonCounts[melon] = 1;
+        } else{
+            melonCounts[melon] += 1;
+        }
+    };
+
+    // return melonCounts
+    console.log(melonCounts)
+}
+
+countMelons(melonsToAdd)
